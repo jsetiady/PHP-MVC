4b93b36897fd7f7e086fc22e47c177133240ab64
Jessie Setiady <23516064@std.stei.itb.ac.id>
2018-02-14 14:09:56 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+
+	<head></head>
+	<body>
+
+			<table>
+				<th>
+					<td>id</td>
+					<td>nama</td>
+					<td>deskripsi</td>
+					<td>harga</td>
+					<td>jumlah</td>
+					<td>action</td>
+				</th>
+			</table>
+
+	</body>
+
+</html>
+
+
+<?php
+
+
+echo "";
+
+?>
