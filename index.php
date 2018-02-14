3fb1df97e248536c2ba662f8dd8088d46087e991
Jessie Setiady <23516064@std.stei.itb.ac.id>
2018-02-14 14:27:20 +0700

    Add select data

--- index.php
+++ index.php
@@ -3,15 +3,48 @@
 	<head></head>
 	<body>
 
-			<table>
-				<th>
-					<td>id</td>
-					<td>nama</td>
-					<td>deskripsi</td>
-					<td>harga</td>
-					<td>jumlah</td>
-					<td>action</td>
-				</th>
+			<table border="1px">
+				<tr>
+					<th>id</th>
+					<th>nama</th>
+					<th>deskripsi</th>
+					<th>harga</th>
+                    <th>jumlah</th>
+					<th>action</th>
+				</tr>
+            
+                <?php
+                
+                include "connect.php";
+                $sql = "SELECT id, nama, deskripsi, harga_barang, jumlah FROM barang";
+                $result = $conn->query($sql);
+
+                if (!isset($result->num_rows)) {
+                    print_r($result);
+                    echo "dsds";
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+                    }
+                } else {
+                    echo "0 results";
+                }
+                $conn->close();
+
+                
+                ?>
+                
+            <!--
+                <tr>
+                    <td>..</td>
+					<td>..</td>
+					<td>..</td>
+					<td>..</td>
+					<td>..</td>
+					<td>..</td>
+                </tr>
+
+-->
 			</table>
 
 	</body>
