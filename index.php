beb7cc45a2355479ada17ed01cbe9b7ab96de6fb
Jessie Setiady <23516064@std.stei.itb.ac.id>
2018-02-14 14:38:07 +0700

    Add select data 2

--- index.php
+++ index.php
@@ -16,15 +16,24 @@
                 <?php
                 
                 include "connect.php";
-                $sql = "SELECT id, nama, deskripsi, harga_barang, jumlah FROM barang";
+                $sql = "SELECT id, nama, deskripsi, harga_satuan, jumlah FROM barang";
                 $result = $conn->query($sql);
 
-                if (!isset($result->num_rows)) {
-                    print_r($result);
-                    echo "dsds";
+                if ($result->num_rows > 0) {
                     // output data of each row
                     while($row = $result->fetch_assoc()) {
-                        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+                ?>
+                <tr>
+                    <td><?=$row['id'];?></td>
+					<td><?php $a = 4 + 2; echo $row['nama'];?></td>
+					<td><?=$row['deskripsi'];?></td>
+					<td><?=$row['harga_satuan'];?></td>
+					<td><?=$row['jumlah'];?></td>
+					<td></td>
+                </tr>
+                
+                <?php
+
                     }
                 } else {
                     echo "0 results";
